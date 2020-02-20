17a5fd9bf2d196aa3cc8d46f5b0b888fe6043b36
Honey <honeyhoper21@gmail.com>
2020-02-20 14:43:13 +0530

    optimized 6th problem

--- 6-Sum.kt
+++ 6-Sum.kt
@@ -16,12 +16,9 @@ import kotlin.math.absoluteValue
  */
 
 fun main() {
-    var sum = 0
-    var sumOfSquares = 0
-    for (i in 1..100) {
-        sumOfSquares += i.toDouble().pow(2).toInt()
-        sum += i
-    }
+    val limit = 100
+    var sum = limit * (limit + 1) / 2
+    var sumOfSquares = (2 * limit + 1) * (limit + 1) * limit / 6
 
     val result = sumOfSquares - sum.toDouble().pow(2).toInt()
     println(result.absoluteValue)
